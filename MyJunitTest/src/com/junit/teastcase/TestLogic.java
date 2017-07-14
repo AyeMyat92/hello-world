a1f7f01019dcc63e38db075b63f2caa618265afd
AyeMyat92 <waterfall.amtz@gmail.com>
2017-07-14 08:47:19 +0000

    Update TestLogic.java

--- MyJunitTest/src/com/junit/teastcase/TestLogic.java
+++ MyJunitTest/src/com/junit/teastcase/TestLogic.java
@@ -9,6 +9,6 @@ public class TestLogic {
 	@Test
 	public void testFindMax(){  
         assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));  
-        assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));  
+        assertEquals(0,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));  
     }  
 }
